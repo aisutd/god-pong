11c1daf42dcebcbff3f323c606633c4abb60b3f5
Michael Spencer <40516460+michael-a-spencer@users.noreply.github.com>
2019-07-29 16:22:52 -0500

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -1,34 +1,50 @@
-# -*- coding: utf-8 -*-
 import random
 import gym
 import numpy as np
 from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential, load_model, model_from_json
+from keras.layers import Dense, Conv2D, Flatten
 from keras.optimizers import Adam
+import cv2 as cv
+import json
 
 EPISODES = 1000
+MAX_FRAMES = 10 ** 7
+IMAGE_SEQUENCE_SIZE = 4
+SCORES = []
+METRIC_STATES = []
+path = "save/"
+
+
+def preprocess_image(img):
+    img = np.dot(img[..., :3], [0.299, 0.587, 0.114])
+    img = cv.resize(img, dsize=(84, 110), interpolation=cv.INTER_AREA)
+    img = img[16:100]
+    return img
+
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.memory = deque(maxlen=10 ** 6)
+        self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.epsilon_min = 0.1
+        self.epsilon_decay = (self.epsilon - self.epsilon_min) / 10 ** 6
+        self.learning_rate = 0.00025
         self.model = self._build_model()
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
+        model.add(Conv2D(filters=16, kernel_size=(8, 8), strides=4, data_format='channels_first', activation='relu'))
+        model.add(Conv2D(filters=32, kernel_size=(4, 4), strides=2, data_format='channels_first', activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(units=256, activation='relu'))
+        model.add(Dense(units=self.action_size, activation='linear'))
         model.compile(loss='mse',
-                      optimizer=Adam(lr=self.learning_rate))
+                      optimizer='rmsprop')
         return model
 
     def remember(self, state, action, reward, next_state, done):
@@ -51,40 +67,69 @@ class DQNAgent:
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+            self.epsilon -= self.epsilon_decay
 
-    def load(self, name):
-        self.model.load_weights(name)
+    def load(self):
+        with open(path + 'model_in_json.json', 'r') as f:
+            model_json = json.load(f)
 
-    def save(self, name):
-        self.model.save_weights(name)
+        self.model = model_from_json(model_json)
+        self.model.load_weights(path + 'model_weights.h5')
+        self.model.compile(loss='mse', optimizer='rmsprop')
+
+    def save(self):
+        model_json = self.model.to_json()
+        with open(path + "model_in_json.json", "w") as json_file:
+            json.dump(model_json, json_file)
+        self.model.save_weights(path + "model_weights.h5")
+        # self.model.save(path + "model.h5")
 
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
+    env = gym.make('BreakoutDeterministic-v4')
+    state = preprocess_image(env.reset())
+    state_size = [84, 84]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
+
+    del agent.model
+    agent.load()
+
     done = False
     batch_size = 32
+    timesteps = 0
+    image_sequence = []
 
     for e in range(EPISODES):
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
-        for time in range(500):
+        if timesteps >= MAX_FRAMES:
+            break
+        score = 0
+        state = preprocess_image(env.reset())
+        state = np.reshape(state, [1, 1] + state_size)
+        action = None
+        for time in range(MAX_FRAMES):
+            timesteps += 1
             # env.render()
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
+            score += reward
             reward = reward if not done else -10
-            next_state = np.reshape(next_state, [1, state_size])
+            next_state = preprocess_image(next_state)
+            image_sequence.append(next_state)
+            if len(image_sequence) > IMAGE_SEQUENCE_SIZE:
+                image_sequence.pop(0)
+                current_state = np.stack([image_sequence[0], image_sequence[1], image_sequence[2], image_sequence[3]])
+            next_state = np.reshape(next_state, [1, 1] + state_size)
             agent.remember(state, action, reward, next_state, done)
             state = next_state
             if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
+                print("episode: {}, total timesteps: {}, score: {}, e: {:.2}"
+                      .format(e, timesteps, score, agent.epsilon))
+                SCORES.append(score)
+
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
+        if e % 10 == 0:
+            agent.save()
+    agent.save()
